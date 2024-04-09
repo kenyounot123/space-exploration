b459f2cc88463df70bf9819816edec2f3cbc59c2
Ken Lu <kenlu519@gmail.com>
2024-04-09 17:16:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Space exploration interactive website
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+function App() {
+  // gsap.registerPlugin(useGSAP);
+
+  // const container = useRef();
+
+  // useGSAP(() => {
+  //   // gsap code here...
+  //   gsap.to(".logo", { x: 360, scale: 2, duration: 2, repeat: -1, yoyo: true }); // <-- automatically reverted
+  // }); // <-- scope is for selector text (optional)
+
+  return <></>;
+}
+
+export default App;
